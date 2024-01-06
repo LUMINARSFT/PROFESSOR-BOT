e2e62ef172985f47eef3ce7e0bad286f0af97cf1
LUMINARSFT <91069152+LUMINARSFT@users.noreply.github.com>
2024-01-07 01:26:10 +0530

    Update password.py

--- plugins/ExtraMods/password.py
+++ plugins/ExtraMods/password.py
@@ -15,5 +15,5 @@ async def password(bot, update):
     limit = int(qw)
     random_value = "".join(random.sample(password, limit))
     txt = f"<b>Lɪᴍɪᴛ:</b> {str(limit)} \n<b>Pᴀꜱꜱᴡᴏʀᴅ: <code>{random_value}</code>"
-    btn = InlineKeyboardMarkup([[InlineKeyboardButton('Sl Media Network™️', url='https://t.me/@sinhalafilmsgo')]])
+    btn = InlineKeyboardMarkup([[InlineKeyboardButton('Sl Media Network™️', url='https://t.me/sinhalafilmsgo')]])
     await message.edit_text(text=txt, reply_markup=btn, parse_mode=enums.ParseMode.HTML)
